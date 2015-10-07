f5d2a5cb9f25464e2644052497395011d4ee123d
Happyman1974 <shurubov@gmail.com>
2015-10-07 21:14:41 +0300

    Create lesson7.cpp
    phonebook

---
+++ lesson7.cpp
@@ -0,0 +1,48 @@
+//phonebook
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Number
+{
+public:
+	string name;
+	string number;
+	Number(const Number &v):
+		name(v.name),
+		number(v.number) {}
+	Number() {}
+	Number &operator=(const Number &v)
+	{
+	name  = v.name;
+	number = v.number;
+	return v;
+	}
+
+};
+
+int main()
+{
+	fstream f("file.txt");
+	vector<Number> telBook;
+	while(true)
+	{
+		Number t;
+		f >> t.name;
+		if (f.eof())
+			break;
+		f >> t.number;
+		if (f.eof())
+			break;
+		telBook.push_back(t);
+	}
+	while(true)
+	{
+		cout << "Input name or stop";
+		string name;
+		cin >> name;
+		for (int i = 0;)
+	}
+}
