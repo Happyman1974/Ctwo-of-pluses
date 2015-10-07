ec73284021e8ed65d5068c8a9d1dabf17dd54593
Happyman1974 <shurubov@gmail.com>
2015-10-07 21:13:16 +0300

    Create lesson5.cpp
    read from / write in file

---
+++ lesson5.cpp
@@ -0,0 +1,31 @@
+// read from / write in file
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ifstream fi("file.txt");
+	while (true)
+	{
+		string v;
+		fi >> v;
+		if (!fi.eof())
+			cout << v << endl;
+		else
+			break;
+	}
+
+	ofstream fo("ofile.txt");
+        for (int i = 0; i < 3; ++i)
+        {
+                string v;
+		cout << "Ввведите текст " << i+1 << " из 3х " << endl;
+		cin >> v;
+                fo << v << endl;
+        }
+
+
+}
