a3b01b6c02b757b2b892ab615903d2b098c0f13e
Happyman1974 <shurubov@gmail.com>
2015-10-07 21:12:31 +0300

    Create lesson4.cpp
    bulevo

---
+++ lesson4.cpp
@@ -0,0 +1,27 @@
+// bulevo
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	if (false && true)
+		cout << "true" << endl;
+	else 
+		cout << "false" << endl;
+cout << "Проехала поливальная машина(y/n)?";
+char a;
+cin >> a;
+bool p = (a == 'y');
+cout << "Был дождь(y/n)?";
+cin >> a;
+bool r = (a == 'y');
+cout << "Светит солнце(y/n)?";
+cin >> a;
+bool s = (a == 'y');
+
+if ((p || r) && !s)
+	cout << "Асфальт мокрый" << endl;
+else
+	cout << "Асфальт сухой" << endl;
+}
